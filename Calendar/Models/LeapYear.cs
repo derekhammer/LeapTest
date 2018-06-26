48e6e395d30407bf7e7137cac1577db6f739a449
Derek Hammer and James Hanley <derek@>
2018-06-26 09:30:33 -0700

    Add Readme, BDD all tests pass

---
+++ Calendar/Models/LeapYear.cs
@@ -0,0 +1,33 @@
+namespace Calendar
+{
+  public class LeapYear
+  {
+    public bool IsLeapYear(int year)
+    {
+      if (year % 400 == 0)
+      {
+          return true;
+      }
+      else if(year % 100 == 0)
+      {
+          return false;
+      }
+      else
+      {
+          return year % 4 == 0;
+      // Intentional Bug if(year % 400 == 0)
+      // {
+      //   return true;
+      // }
+      // else if (year % 100 == 0)
+      // {
+      //   return false;
+      // }
+      // else
+      // {
+      //   return year % .4 == 0;
+      // }
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+#By Derek Hammer & James Hanley
+Short project on Organizing Red Green Refactoring and BDD with LeapYear.
+
+##BDD
+|Behavior|Input|Output|
+|--------|-----|------|
+|LeapYear divisible by 4|2012|True|
+|LeapYear not divisible by 4|1999|False|
+|LeapYear Multiple of 100|1900|False|
+|LeapYear Multiple of 400|2000|True|
